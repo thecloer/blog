f12a054f129f3e48da2b5e5bd25b1fc825a6a865
thecloer <chltjrrb97@gmail.com>
2024-08-12 19:41:08 +0900

    add: basic settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,23 @@
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
+import siteConfig from '@/configs/site';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+export const metadata: Metadata = {
+  title: siteConfig.title,
+  description: siteConfig.description,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? siteConfig.url),
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: 'prefers-color-scheme: light', color: 'white' },
+    { media: 'prefers-color-scheme: dark', color: 'black' },
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -6,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='ko' className='scroll-pt-[--heading-height]'>
+      <body className={cn('min-h-dvh bg-background font-sans antialiased', inter.variable)}>{children}</body>
     </html>
   );
 }
---
+++ src/configs/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  title: "Thecloer's Blog",
+  url: 'https://blog.thecloer.com',
+  description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+  author: 'thecloer',
+  email: 'chltjrrb97@gmail.com',
+  links: {
+    twitter: 'https://twitter.com/jollyshopland',
+    github: 'https://github.com/jolbol1',
+    linkedIn: 'https://jamesshopland.com',
+  },
+} as const;
+
+export default siteConfig;
