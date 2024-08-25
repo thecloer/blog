7ba85851cd06994636041fe0f387e2362814ac74
thecloer <chltjrrb97@gmail.com>
2024-08-26 03:26:58 +0900

    add: tailwind theme.extend mobile media query 480px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,9 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      screens: {
+        mobile: '480px',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
